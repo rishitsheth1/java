96b896f5c6fa6017a2b0bd69625490ac538206d8
rishitsheth1 <rsheth618@gmail.com>
2016-09-09 20:10:02 -0400

    Initial Commit
    no description

---
+++ Welcome2.java
@@ -0,0 +1,8 @@
+public class Welcome2
+{
+	public static void main(String[] args)
+	{
+        System.out.println("Java Programming!");
+        System.out.print("Welcome to ");
+	}
+}
---
+++ rishit.java
@@ -0,0 +1,12 @@
+//this class is Welcome
+public class rishit
+{
+public static void main(String[] Args)
+{
+	{	
+        System.out.print("hi ");
+		System.out.println("Welcome to\n \n \n Java \n Programming!");
+        System.out.printf("%s %n %s %s %d ", "Welcome to ",96 ,"hi",7);
+	}
+}
+}
